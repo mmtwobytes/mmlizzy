63ecf4b96715fe0be5a975c4c592673d54eec674
1XusernameX1 <87429875+1XusernameX1@users.noreply.github.com>
2021-08-05 03:09:22 -0700

    Create list.js

---
+++ plugins/list.js
@@ -0,0 +1,33 @@
+const XTroid = require('../events');
+
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+
+const Config = require('../config');
+
+XTroid.addCMD({pattern: 'lll', fromMe: true, deleteCommand: true,}, (async (message, match) => {
+
+var rows = [
+
+ {title: 'Row 1', description: "Hello it's description 1", rowId:"rowid1"},
+
+ {title: 'Row 2', description: "Hello it's description 2", rowId:"rowid2"}
+
+]
+
+var sections = [{title: "Section 1", rows: rows}]
+
+var button = {
+
+ buttonText: 'Click Me!',
+
+ description: "Hello it's list message",
+
+ sections: sections,
+
+ listType: 1
+
+}
+var id = '94702102324@s.whatsapp.net'
+await message.client.sendMessage(id, button, MessageType.listMessage)
+
+    }));
